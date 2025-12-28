acc8fb843e76fd21b78d986c15b13de573bc9718
Samuel <41825140+Tarawally@users.noreply.github.com>
2025-12-28 00:45:26 +0000

    feat(explorer): implement sorting by frontmatter weight

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -30,10 +30,11 @@ const defaultOptions: Options = {
     return node
   },
   sortFn: (a, b) => {
-    // Sort order: folders first, then files. Sort folders and files alphabeticall
+    // Sort order: folders first, then files. Sort folders and files by weight, then alphabetically
     if ((!a.isFolder && !b.isFolder) || (a.isFolder && b.isFolder)) {
-      // numeric: true: Whether numeric collation should be used, such that "1" < "2" < "10"
-      // sensitivity: "base": Only strings that differ in base letters compare as unequal. Examples: a ≠ b, a = á, a = A
+      if (a.data?.weight !== b.data?.weight) {
+        return (a.data?.weight ?? 100) - (b.data?.weight ?? 100)
+      }
       return a.displayName.localeCompare(b.displayName, undefined, {
         numeric: true,
         sensitivity: "base",
